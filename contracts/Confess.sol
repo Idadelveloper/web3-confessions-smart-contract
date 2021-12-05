a1e2ad917da908411865327840f3fd92b3e25df3
Ida Delphine <idadelm@gmail.com>
2021-12-05 21:11:29 +0100

    Create simple confess smart contract

---
+++ contracts/Confess.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Confess {
+    constructor() {
+        console.log("Say something you've never told anyone and you'll probably die without telling anybody");
+    }
+}
