b4dea12ecc5e982e2ef9d0b0c52237e3e10170b3
Ida Delphine <idadelm@gmail.com>
2021-12-05 22:23:32 +0100

    Enable other addresses to send confessions

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,25 @@
 const main = async() => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const confessContractFactory = await hre.ethers.getContractFactory('Confess');
     const confessContract = await confessContractFactory.deploy();
     await confessContract.deployed();
+
     console.log("Contract deployed to:", confessContract.address);
+    console.log("Contract deployed to:", confessContract.address);
+
+    let confessCount;
+    confessCount = await confessContract.getTotalConfessions();
+
+    let confessTxn = await confessContract.confess();
+    await confessTxn.wait();
+
+    confessCount = await confessContract.getTotalConfessions();
+
+    confessTxn = await confessContract.connect(randomPerson).confess();
+    await confessTxn.wait();
+
+    confessCount = await confessContract.getTotalConfessions();
+
 };
 
 const runMain = async() => {
