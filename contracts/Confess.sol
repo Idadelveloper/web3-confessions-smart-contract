af6e82d33a32e3462e272d0b4b7e0160e4514c6d
Ida Delphine <idadelm@gmail.com>
2021-12-05 22:05:04 +0100

    Add functions to store confessions

--- contracts/Confess.sol
+++ contracts/Confess.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Confess {
+    uint256 totalConfessions;
+
     constructor() {
         console.log("Say something you've never told anyone and you'll probably die without telling anybody");
+    }
+
+    function confess() public {
+        totalConfessions += 1;
+        console.log("%s has confessed!", msg.sender);
+    }
+
+    function getTotalConfessions() public view returns (uint256) {
+        console.log("We have %d total confessions!", totalConfessions);
+        return totalConfessions;
     }
 }
