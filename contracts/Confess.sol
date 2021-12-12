4d27c7edcb88caa3b8c1a5f3376d21de1ade3dd6
Ida Delphine <idadelm@gmail.com>
2021-12-12 07:06:23 +0100

    Prevent spammers by waiting 15mins

--- contracts/Confess.sol
+++ contracts/Confess.sol
@@ -22,6 +22,8 @@ contract Confess {
 
     Confession[] confessions;
 
+    mapping(address => uint256) public lastConfessedAt;
+
     constructor() payable {
         console.log("We have been constructed!");
         /*
@@ -31,31 +33,28 @@ contract Confess {
     }
 
     function confess(string memory _message) public {
+
+        require(
+            lastConfessedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15m"
+        );
+
+        lastConfessedAt[msg.sender] = block.timestamp;
+
         totalConfessions += 1;
         console.log("%s has confessed!", msg.sender);
 
         confessions.push(Confession(msg.sender, _message, block.timestamp));
 
-        /*
-         * Generate a new seed for the next user that sends a confession
-         */
         seed = (block.difficulty + block.timestamp + seed) % 100;
-        
-        console.log("Random # generated: %d", seed);
 
-        /*
-         * Give a 50% chance that the user wins the prize.
-         */
         if (seed <= 50) {
             console.log("%s won!", msg.sender);
 
-            /*
-             * The same code we had before to send the prize.
-             */
             uint256 prizeAmount = 0.0001 ether;
             require(
                 prizeAmount <= address(this).balance,
-                "Trying to withdraw more money than the contract has."
+                "Trying to withdraw more money than they contract has."
             );
             (bool success, ) = (msg.sender).call{value: prizeAmount}("");
             require(success, "Failed to withdraw money from contract.");
